e2c093bcc998fd3236195f4d575f5e3dcf435cea
bala-amypo <bala@amypo.in>
2025-12-13 10:02:22 +0000

    User commit

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,3 +1,16 @@
+package com.example.demo.controller;
 
-import org.springg
+import org.springframework.web.bind.annotations.GetMapping;
+import org.springframework.web.bind.annotations.RestController;
+
 @RestController
+public class Controller {
+
+    @GetMapping("/simple-status")
+    String print()
+    {
+        return "Credit Card Reward Maximizer is running";
+    }
+
+}
+
