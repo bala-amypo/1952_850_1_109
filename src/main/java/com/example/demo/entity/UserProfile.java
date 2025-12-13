fe0cfa1b2225c2a01a315bee18720f0daa4c456d
bala-amypo <bala@amypo.in>
2025-12-13 09:18:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -18,7 +18,10 @@ public class UserProfile
     @Column(name = "user_id",unique = true , nullable = false)
     private String userid;
 
-    @Column(name = )
+    @Column(name = "full_name" , nullable = false)
+    private String fullName;
+
+    
 
 
 }
