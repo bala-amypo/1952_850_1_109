3314620d6964f1e69915b98409904e06402830e5
bala-amypo <bala@amypo.in>
2025-12-26 12:28:26 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,3 +1,13 @@
+package com.example.demo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
